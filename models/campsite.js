6c5d77250070ca3144f308d57ad5056e3ccbe6b1
Dani <ddw29@me.com>
2021-11-15 21:51:07 -0500

    Express REST API with MongoDB and Mongoose Part 1

---
+++ models/campsite.js
@@ -0,0 +1,65 @@
+// require('mongoose-currency').loadType(mongoose);
+// const Currency = mongoose.Types.Currency;
+
+//where we will define schema and models for the campsite database
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const campsiteSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: { //contains a path to the image 
+        type: String,
+        required: true
+    },
+    elevation: { 
+        type: Number,
+        required: true
+    },
+    cost: {
+        type: String,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    comments: [commentSchema] //comment sub-document
+}, {
+    timestamps: true
+});
+//first argument is the object of required properties of the Schema , second argument is optional that will add a timestamp and will be given "created at" and "updated at" properties
+
+const Campsite = mongoose.model('Campsite', campsiteSchema) //Campsite Model is created and first argument is "Campsite" of the name of collection to be used for this model. Mongo will look for lower case plural version for the collection. Then pass in the Schema used for this collection 
+
+//for a Model, a constructor function is returned
+
+module.exports = Campsite;
--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,55 +1,81 @@
 //contains routes for all of the campsite data 
 
 const express = require('express');
+const Campsite = require('../models/campsite');
+//updating so each method can interact with the mongoDB server and model
 const campsiteRouter = express.Router();
 
 campsiteRouter.route('/')  //drop the app word and path from parameter to chain all the routes together and remove semicolon at the end (unless last method on chain)
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
+.get((req, res, next) => { //next function is an argument
+    Campsite.find() //static method that will query database for all docs using the campsite model
+    .then(campsites => { //access the results from .find()
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsites); //will send json data to client and close client
+    })
+    .catch(err => next(err)); //pass off error to overall handle error built into express
 })
-.get((req, res) => {
-    res.end('Will send all the campsites to you');
+.post((req, res, next) => {
+    Campsite.create(req.body) //create new campsite doc and save
+    .then(campsite => {
+        console.log('Campsite Created ', campsite);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsite); //mongoose will check to make sure it the new doc the previously defined schema
+    })
+    .catch(err => next(err));
 })
-.post((req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
-})
+
 .put((req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /campsites');
 })
-.delete((req, res) => {
-    res.end('Deleting all campsites');
-}) //methods are chained together instead of 5 separate routes
+.delete((req, res, next) => {
+    Campsite.deleteMany() //will delete every document in the campsite collection
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
+}); //methods are chained together instead of 5 separate routes
 
 //campsiteID requests
-campsiteRouter.route('/:campsiteId')  
-
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-
-.get((req, res) => {
-    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+campsiteRouter.route('/:campsiteId')
+.get((req, res, next) => {
+    Campsite.findById(req.params.campsiteId) //findById is a Mongoose method
+    .then(campsite => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsite);
+    })
+    .catch(err => next(err));
 })
 
 .post((req, res) => {
     res.statusCode = 403;
     res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
 })
-
-.put((req, res) => {
-    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-    res.end(`Will update the campsite: ${req.body.name}
-        with description: ${req.body.description}`);
+.put((req, res, next) => {
+    Campsite.findByIdAndUpdate(req.params.campsiteId, {
+        $set: req.body
+    }, { new: true })
+    .then(campsite => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsite);
+    })
+    .catch(err => next(err));
 })
 
-.delete((req, res) => {
-    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+.delete((req, res, next) => {
+    Campsite.findByIdAndDelete(req.params.campsiteId)
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
 });
 
-
-module.exports = campsiteRouter; //export campsiteRouter to use in the server.js
+module.exports = campsiteRouter;//export campsiteRouter to use in the server.js
