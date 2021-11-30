724b7b8c7da514c886414b0e655f49e4bf836e5a
Dani <ddw29@me.com>
2021-11-30 18:21:57 -0500

    Week 4 Workshop Assignment:

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '641308170617871',
+        clientSecret: '75b92fed4509194215b1eeca331d5fb2'
+    }
 }
